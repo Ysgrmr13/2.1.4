ed481b04d3df3e9dc48f2d282990f414f2c19749
Ysgrmr13 <Ysgrmr13@gmail.com>
2024-12-16 15:07:27 +0300

    Created link on 8 steps

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,14 +1,15 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-    private Egg6 egg;
 
-    @Autowired
-    public Duck5(Egg6 egg) {this.egg = egg;}
+    private final Egg6 egg;
+
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
 
     @Override
     public String toString() {
